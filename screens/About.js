60aab443d8848f986cc24b4f6f3500e81007f478
yasmin <yasminefawzy22@gmail.com>
2019-11-28 23:02:46 +0100

    style about page

--- screens/About.js
+++ screens/About.js
@@ -12,12 +12,60 @@ export default function About(props) {
       <Headers
         name='About'
         navigation={props.navigation} />
-        <View style ={{borderWidth: 3, borderColor: 'grey', backgroundColor:'transparent', margin: 5}}> 
-    {/* team members and avatar */}
-        <View style={{height: 310, margin: 12}}>
+        <View style ={{backgroundColor:'#f8f8ff', margin: 5}}> 
+    
+    {/* About GeoQuest */}
+    <View style={{height: 290, margin: 12}}>
+      <Text style ={styles.text}>About :</Text>
+      
+      <Text style={styles.txtdetails}>- GeoQuest is a simple and powerful Geocaching app for iOS and Android that includes the following features:</Text>
+      <Text style={styles.txtdetails}>- GeoQuest account</Text>
+      <Text style ={{marginLeft: 25,lineHeight: 20}}>uses the Geocaching Live API so that you can login with your account and see all of your information</Text>
+      <Text style={styles.txtdetails}>- Find Caches :</Text>
+      <Text style ={{marginLeft: 25, lineHeight: 20}}>Current location, coordinates, search for a location</Text>
+      <Text style={styles.txtdetails}>- Map Options :</Text>
+      <Text style ={{marginLeft: 25, lineHeight: 20}}>Choose from Apple Maps, Google Maps, OpenStreet Map</Text>
+      
+
+      {/* <Image  source={require('../assets/geo.png')}
+              style={styles.image}/> */}
+      </View>
+      <Divider style ={{backgroundColor:'#d3d3d3', height: 1, marginLeft:22, marginRight: 22}} />
+      {/* technologies container */}
+      <View style={{height: 170, margin: 12}}>
+      <Text style ={styles.text}>Technologies:</Text>
+      <View style ={{marginLeft: 20, marginTop: 10}}>
+      <Text style={{lineHeight: 30}}>- React Native </Text>
+      <Text style={{lineHeight: 30}}>- Node.js</Text>
+      <Text style={{lineHeight: 30}}>- Express</Text>
+      <Text style={{lineHeight: 30}}>- Mongo</Text>
+      </View>
+      </View>
+
+      <Divider style ={{backgroundColor:'#d3d3d3', height: 1, marginLeft:22, marginRight: 22}} />
+
+       {/* Future Implementations container */}
+
+
+       <View style={{height: 250, margin: 12}}>
+      <Text style ={styles.text}>Future Implementations:</Text>
+      <View style ={{marginLeft: 20, marginTop: 10}}>
+          <Text style={{lineHeight: 30}}>- offline Maps</Text>
+          <Text style={{lineHeight: 30}}>- Share location with friends</Text>
+          <Text style={{lineHeight: 30}}>- Chat</Text>
+          <Text style={{lineHeight: 30}}>- Friend lists</Text>
+          <Text style={{lineHeight: 30}}>- Upload images,Request images, profile image</Text>
+          <Text style={{lineHeight: 30}}>- Bookmark lists</Text>
+
+          </View>
+      </View>
+      <Divider style ={{backgroundColor:'#d3d3d3', height: 1, marginLeft:22, marginRight: 22}} />
+
+      {/* team members and avatar */}
+      <View style={{height: 340, margin: 12}}>
       <Text style ={styles.text}>Team Members :</Text>
 
-    <View style ={{flex: 1, flexDirection:'row', flexWrap:'wrap', justifyContent:'space-around', marginRight:30, marginTop: 4}}>
+    <View style ={{flex: 1, flexDirection:'row', flexWrap:'wrap', justifyContent:'space-around', marginRight:30, marginTop: 10}}>
       
       <View >
       <Avatar  rounded size={120}
@@ -43,55 +91,9 @@ export default function About(props) {
         </View>
     </View>
     </View>
-    <Divider style ={{backgroundColor:'#d3d3d3'}} />
+    <Divider style ={{backgroundColor:'#d3d3d3', height: 1, marginLeft:22, marginRight: 22}} />
 
 
-    {/* About GeoQuest */}
-    <View style={{height: 250, margin: 12}}>
-      <Text style ={styles.text}>About :</Text>
-      
-      <Text style={styles.txtdetails}>- GeoQuest is a simple and powerful Geocaching app for iOS and Android that includes the following features:</Text>
-      <Text style={styles.txtdetails}>- GeoQuest account</Text>
-      <Text style ={{marginLeft: 25}}>uses the Geocaching Live API so that you can login with your account and see all of your information</Text>
-      <Text style={styles.txtdetails}>- Find Caches :</Text>
-      <Text style ={{marginLeft: 25}}>Current location, coordinates, search for a location, or GC code.</Text>
-      <Text style={styles.txtdetails}>- Map Options :</Text>
-      <Text style ={{marginLeft: 25}}>Choose from Apple Maps, OpenStreet Map online</Text>
-      
-
-      <Image  source={require('../assets/geo.png')}
-              style={styles.image}/>
-      </View>
-      <Divider style ={{backgroundColor:'#d3d3d3'}} />
-      {/* technologies container */}
-      <View style={{height: 160, margin: 12}}>
-      <Text style ={styles.text}>Technologies:</Text>
-      <View style ={{marginLeft: 20, marginTop: 10}}>
-      <Text>- React Native </Text>
-      <Text>- Node.js</Text>
-      <Text>- Express</Text>
-      <Text>- Mongo</Text>
-      </View>
-      </View>
-
-      <Divider style ={{backgroundColor:'#d3d3d3'}} />
-
-       {/* Future Implementations container */}
-
-
-       <View style={{height: 200, margin: 12}}>
-      <Text style ={styles.text}>Future Implementations:</Text>
-      <View style ={{marginLeft: 20, marginTop: 10}}>
-          <Text>- offline Maps</Text>
-          <Text>- Share location with friends</Text>
-          <Text>- Chat</Text>
-          <Text>- Friend lists</Text>
-          <Text>- Upload images,Request images, profile image</Text>
-          <Text>- Bookmark lists</Text>
-
-          </View>
-      </View>
-      <Divider style ={{backgroundColor:'#d3d3d3', height: 2}} />
 
       </View>
     </ScrollView>
@@ -127,14 +129,14 @@ const styles = StyleSheet.create({
 
   },
   text: {
-    marginTop: 10,
+    marginTop: 7,
     fontSize: 20,
     fontWeight: 'bold',
     alignItems:'flex-start',
     marginLeft: 19,
   },
 
-  image: {
+ /*  image: {
     width:50,
     height:50,
     alignSelf: 'flex-end',
@@ -144,10 +146,10 @@ const styles = StyleSheet.create({
     // backgroundColor: 'black',
     // borderRadius: 3
 
-  },
+  }, */
   txtdetails: {
     marginLeft: 10,
     marginTop: 12,
-    
+    lineHeight: 20
   }
 })
