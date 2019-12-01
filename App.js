5a334c7c381355ee8bcffbf9c2b44e2a8772fa1c
NAQVI <nomanalam7214@gmail.com>
2019-12-01 21:00:35 +0100

    added globalstate and useReducer hook

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React,{useReducer} from 'react';
+import { StyleSheet, View  } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import PlayMode from './firstscreen/PlayMode';
 import HomePage from './firstscreen/HomePage'
 import ExploreMode from './firstscreen/ExploreMode';
+import GlobalState from "./globalstate/GlobalState"
+import Reducer,{initialstate} from "./globalstate/Reducer"
+export default  App =()=> {
 
-export default class App extends React.Component {
 
-  render() {
+  const [state,dispatch]=useReducer(Reducer,initialstate)
+ 
+  const switchValue=(value)=>{
+    dispatch({type:"switch",payload:value})
+  }
+
     return (
+      <GlobalState.Provider value={{state,switchValue}}> 
       <View style={styles.view} >
         <Appcontainer />
       </View>
+      </GlobalState.Provider>
     );
-  }
 }
 
 const appSwitchNavigation = createSwitchNavigator({
