747ae6ca8e3de6328c4264ffc75ded2dd307c7f1
NAQVI <nomanalam7214@gmail.com>
2019-12-01 12:36:12 +0100

    creacted Stack navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,63 +1,33 @@
-import React,{useState} from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Headers from '../components/Headers'
-
-import SubHeaderButtons from '../components/SubHeaderButtons';
-import Map from '../components/Map';
+import React from 'react';
+import {View} from 'react-native';
+import Mapscreen from "./Mapscreen"
+import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from "react-navigation-stack" 
+import LoginView from './registeration/login';
+import SignupView from "./registeration/signup"
 
 export default function HomeScreen(props) {
-
-  const [check,setcheck]=useState(true)
-  const [mode,setmode]=useState("explore")
-
-  const changeMode=()=>{
-    setcheck(!check);
-    if(check){
-      setmode("play")
-    }else{
-      setmode("explore")
-    }
-    
-  }
   return (
-    <View style={{ flex: 1 }}>
-      <Headers
-        name='Home Screen'
-        navigation={props.navigation}
-      />
-      <SubHeaderButtons changeMode={changeMode} mode={mode}/>
-      <Map mode={mode} />
+    <View style={{ flex: 1 }}> 
+      <Appcontainer/>
     </View>
   )
 }
 
-HomeScreen.navigationOptions = {
-  drawerIcon: () => (
-    <View style={styles.icon}>
-      <Ionicons name="md-home"
-        size={24}
-        style={styles.iconcolor}
-      />
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  icon: {
-    fontSize: 20,
-    color: "white",
-    textAlign: 'center',
-    backgroundColor: 'green',
-    height: 25,
-    width: 25,
-    borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  iconcolor: {
-    color: "white",
-
-  },
-
+const AppSwitchNavigation = createStackNavigator({
+  Mapscreen: {screen: (props)=><Mapscreen {...props}/>,  navigationOptions: {
+    header: null
+  }},
+  LoginView:{screen: LoginView,  navigationOptions: {
+    title:"Login"
+  }},
+  SignupView:{screen: SignupView,  navigationOptions: {
+    title:"SignUp"
+  }},
 })
+
+const Appcontainer = createAppContainer(AppSwitchNavigation)
+
+
+
+
