99043471f88d6d6865e4ddb2bcef017421071339
NAQVI <nomanalam7214@gmail.com>
2019-12-08 20:37:10 +0100

    added on more screen in stacknav

--- screens/stacknavforcreatequest.js/stacknavQuest.js
+++ screens/stacknavforcreatequest.js/stacknavQuest.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import MainQuest from "./mainQuest"
+import Point from "./point"
 
 const AppNavigator = createStackNavigator({
   Home: {
     screen: MainQuest,
+  },
+  Point: {
+    screen: Point,
   },
 },
 {
