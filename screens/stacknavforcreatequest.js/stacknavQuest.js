0e4374f72efd5ae94de7f5ca8f25d9d7a331188b
NAQVI <nomanalam7214@gmail.com>
2019-12-10 16:05:44 +0100

    added one more screen

--- screens/stacknavforcreatequest.js/stacknavQuest.js
+++ screens/stacknavforcreatequest.js/stacknavQuest.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import MainQuest from "./mainQuest"
-import Point from "./point"
+import SelectMap from "./SelectMap"
+import PointDestination from "./PointDestination"
 
 const AppNavigator = createStackNavigator({
   Home: {
     screen: MainQuest,
   },
-  Point: {
-    screen: Point,
+  SelectMap: {
+    screen: SelectMap,
   },
+  PointDestination:{
+    screen:PointDestination
+  }
 },
 {
     headerMode: 'none',
